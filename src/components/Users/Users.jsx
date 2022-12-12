b2be29664b9a16d01af15774f67dc229eae4e845
vovka8101 <vova.sem143@gmail.com>
2022-12-12 12:31:55 +0200

    class component (lesson 53)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,30 @@
 import s from './Users.module.css';
 import axios from 'axios';
 import userAva from '../../assets/images/userAva.jpg';
+import React from 'react';
 
-const Users = (props) => {
-
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-        props.setUsers(response.data.items);
-      });
+      this.props.setUsers(response.data.items);
+    });
   }
-
-  return (
-    <div className={s.content}>
-      <h2>Users</h2>
-      {props.users.map(el => {
-        return (
-          <div className={s.container}>
-            <div className={s.userPhoto}>
-              <div className={s.ava}><img src={el.photos.small !== null ? el.photos.small : userAva} alt="User ava" /></div>
-              {el.followed
-                ? <button onClick={() => props.unfollow(el.id)}>Unfollow</button>
-                : <button onClick={() => props.follow(el.id)}>Follow</button>}
-            </div>
-            <div className={s.userDescription}>
+  
+  render() {
+    return (
+      <div className={s.content}>
+        <h2>Users</h2>
+        {this.props.users.map(el => {
+          return (
+            <div className={s.container}>
+              <div className={s.userPhoto}>
+                <div className={s.ava}><img src={el.photos.small !== null ? el.photos.small : userAva} alt="User ava" /></div>
+                {el.followed
+                  ? <button onClick={() => this.props.unfollow(el.id)}>Unfollow</button>
+                  : <button onClick={() => this.props.follow(el.id)}>Follow</button>}
+              </div>
+              <div className={s.userDescription}>
                 <div className={s.col1}>
                   <div className={s.userName}><span className={s.userFullName}>{el.name}</span> <span className={s.userNickname}>({"nickname"})</span></div>
                   <div className={s.userStatus}>{el.status !== null ? el.status : "N/A"}</div>
@@ -31,12 +33,13 @@ const Users = (props) => {
                   <div className={s.userCountry}>{"country"},</div>
                   <div className={s.userCity}>{"city"}</div>
                 </div>
+              </div>
             </div>
-          </div>
-        )
-      })}
-    </div>
-  )
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default Users;
